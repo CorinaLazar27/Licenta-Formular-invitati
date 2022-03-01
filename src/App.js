5f9162fac2e316c6c8fa4c8937d973bcb2be9ad2
CorinaLazar27 <corina_lazar27@yahoo.com>
2022-03-01 17:21:42 +0200

    Introducere

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { TextField, Grid } from "@mui/material";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Background">
+        <form>
+          <Grid container spacing={2} columns={16}>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+            <Grid item xs={8}>
+              <TextField
+                id="outlined-basic"
+                label="Nume"
+                variant="outlined"
+                required
+              />
+            </Grid>
+          </Grid>
+          <br></br>
+          <input type="submit"></input>
+        </form>
+      </div>
     </div>
   );
 }
